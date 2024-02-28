14a718e00378ab0fb4e9e74f5ae7caee487eca50
nats <139819517+natsloo@users.noreply.github.com>
2024-02-28 13:42:27 +0100

    zad1

--- svg/src/Segment.java
+++ svg/src/Segment.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Segment {
     private Point start;
     private Point end;
@@ -5,6 +7,11 @@ public class Segment {
     public Segment(Point start, Point end) {
         this.start=start;
         this.end = end;
+    }
+
+    public String toSvg(){
+        return String.format(Locale.ENGLISH,
+                "  <line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" style=\"stroke:red;stroke-width:2\" />\n", start.x,start.y,end.x,end.y);
     }
 
     public Point getStart(){
