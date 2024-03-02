a6cf9e28749c54dfb3d92c9892d41f8adb086367
nats <139819517+natsloo@users.noreply.github.com>
2024-03-02 12:49:57 +0100

    zad1

--- svg/src/Segment.java
+++ svg/src/Segment.java
@@ -5,16 +5,35 @@ public class Segment {
     private Point end;
 
     public Segment(Point start, Point end) {
-        this.start=start;
+        this.start = start;
         this.end = end;
+    }
+
+    public Point getStart(){
+        return start;
+    }
+
+    public Point getEnd(){
+        return end;
     }
 
     public String toSvg(){
         return String.format(Locale.ENGLISH,
                 "  <line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" style=\"stroke:red;stroke-width:2\" />\n", start.x,start.y,end.x,end.y);
     }
+    //<line x1="0" y1="0" x2="300" y2="200" style="stroke:red;stroke-width:2" />
     public double length(){
         return Math.hypot(end.x- start.x,end.y-start.y);
+    }
+
+    public static Segment[] perpendicularTo(Segment s, Point p){
+        double dx=s.end.x-s.start.x;
+        double dy=s.end.y-s.start.y;
+
+        return new Segment[]{
+                new Segment(p,new Point(p.x-dy,p.y+dx)),
+                new Segment(p,new Point(p.x+dy, p.y-dx)),
+        };
     }
 }
 
